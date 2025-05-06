8d670569732ac634fc82d045f2659ccf773faf84
sahathiya <salamkaraya4@gmail.com>
2025-05-06 13:11:01 +0530

    set up the real time notification

--- Client/feedbackportal/src/utils/axiosInstance.js
+++ Client/feedbackportal/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000', 
+  baseURL: 'https://echohub-d780.onrender.com', 
   withCredentials: true,
 
   
